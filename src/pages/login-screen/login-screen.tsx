19fc109f38bf65f000f6f680da12ad691970a644
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2025-01-08 14:19:15 +0300

    Fix: Рефакторит форму логина

--- src/pages/login-screen/login-screen.tsx
+++ src/pages/login-screen/login-screen.tsx
@@ -1,9 +1,19 @@
-import Login from '../../components/login';
+import LoginForm from '../../components/login-form';
 
 function LoginScreen(): JSX.Element {
   return (
     <main className="page__main page__main--login">
-      <Login/>
+      <div className="page__login-container container">
+        <section className="login">
+          <h1 className="login__title">Sign in</h1>
+          <LoginForm/>
+        </section>
+        <section className="locations locations--login locations--current">
+          <div className="locations__item">
+            <a className="locations__item-link" href="#"> <span>Amsterdam</span> </a>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
