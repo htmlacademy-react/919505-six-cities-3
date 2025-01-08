e6023a94bf5e4eb999b59244313d3a57d5cc5b0e
KaiZen-prog <60105826+KaiZen-prog@users.noreply.github.com>
2025-01-08 14:25:35 +0300

    Fix: Рефакторит layout.tsx

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,40 +1,12 @@
 import {PropsWithChildren} from 'react';
 import {Outlet, useLocation} from 'react-router-dom';
 import {AppRoute} from '../../utils/const';
+import {getLayoutState} from './utils';
 import Footer from '../footer';
 import Header from '../header';
 
 type LayoutProps = {
   favoritesQuantity: number;
-};
-
-const getLayoutState = (pathName: AppRoute, favoritesQuantity: number) => {
-  let rootClassName = '';
-  let linkClassName = '';
-  let shouldRenderUser = true;
-  let shouldRenderHeader = true;
-  let shouldRenderFooter = false;
-
-  switch (pathName) {
-    case AppRoute.Root:
-      rootClassName = 'page--gray page--main';
-      linkClassName = 'header__logo-link--active';
-      break;
-    case AppRoute.Login:
-      rootClassName = 'page--gray page--login';
-      shouldRenderUser = false;
-      break;
-    case AppRoute.Favorites:
-      rootClassName = favoritesQuantity === 0 ? 'page--favorites-empty' : '';
-      shouldRenderFooter = true;
-      break;
-    case AppRoute.PageNotFound:
-      rootClassName = 'page--gray page--main';
-      shouldRenderHeader = false;
-      break;
-  }
-
-  return {rootClassName, linkClassName, shouldRenderUser, shouldRenderHeader, shouldRenderFooter};
 };
 
 export default function Layout({favoritesQuantity}: PropsWithChildren<LayoutProps>): JSX.Element {
---
+++ src/components/layout/utils.ts
@@ -0,0 +1,30 @@
+import {AppRoute} from '../../utils/const';
+
+export const getLayoutState = (pathName: AppRoute, favoritesQuantity: number) => {
+  let rootClassName = '';
+  let linkClassName = '';
+  let shouldRenderUser = true;
+  let shouldRenderHeader = true;
+  let shouldRenderFooter = false;
+
+  switch (pathName) {
+    case AppRoute.Root:
+      rootClassName = 'page--gray page--main';
+      linkClassName = 'header__logo-link--active';
+      break;
+    case AppRoute.Login:
+      rootClassName = 'page--gray page--login';
+      shouldRenderUser = false;
+      break;
+    case AppRoute.Favorites:
+      rootClassName = favoritesQuantity === 0 ? 'page--favorites-empty' : '';
+      shouldRenderFooter = true;
+      break;
+    case AppRoute.PageNotFound:
+      rootClassName = 'page--gray page--main';
+      shouldRenderHeader = false;
+      break;
+  }
+
+  return {rootClassName, linkClassName, shouldRenderUser, shouldRenderHeader, shouldRenderFooter};
+};
